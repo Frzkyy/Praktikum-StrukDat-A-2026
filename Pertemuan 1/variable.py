5e05c3a6d5b791c0e8083a75093f28c1974ddd56
Frzkyy <fairuzzacky@gmail.com>
2026-01-29 21:41:15 +0700

    revisi first half

--- Pertemuan 1/variable.py
+++ Pertemuan 1/variable.py
@@ -6,49 +6,47 @@ pi = 3.14 #float
 print(f"{nama} berumur {umur} tahun") 
 
 # Casting
-x = str(3)    # x will be '3'
-y = int(3)    # y will be 3
-z = float(3)  # z will be 3.0 
-
 a = 20
 print(a + 30, type(a))
-a = str(a)
+a = str(a) #Ini castingnya
 print(a, type(a))
 
 # Penamaan variable yang benar
 """
-myvar = "John"
-my_var = "John"
-_my_var = "John"
-myVar = "John"
-MYVAR = "John"
-myvar2 = "John"
+inivariabel = "satu"
+ini_variabel = "dua"
+_ini_variabel = "tiga"
+iniVariabel = "empat"
+INIVARIABEL = "lima"
+inivariabel67 = "enam"
 """
 
 # Penamaan variable yang salah
 """
-2myvar = "John"
-my-var = "John"
-my var = "John"
+67inivariabel = "John" (angka tidak boleh ada di depan kalimat)
+ini-variabel = "John" (gk boleh ada simbol selain underscore)
+ini variabel = "John" (jangan ada spasi diantara kita)
 """
 
 # jenis penamaan variable
 """
 1. Camel Case
-    = myVariableName = "John"
+    = iniMyVariabelSaya = "John"
 2. Pascal Case
-    MyVariableName = "John"
+    IniMyVariabelSaya = "John"
 3. Snake Case
-    my_variable_name = "John"
+    ini_my_variabel_saya = "John"
 """
 # Assign banyak variable sekaligus
-x, y, z = "Orange", "Banana", "Cherry"
-print(x)
-print(y)
-print(z)
+siswa1, siswa2, siswa3 = 80, 70, 60
+print(f"siswa1 nilainya {siswa1}")
+print(f"siswa2 nilainya {siswa2}")
+print(f"siswa3 nilainya {siswa3}")
 
 # Assign banyak variable tapi satu value
-x = y = z = "Orange"
-print(x)
-print(y)
-print(z)
+rusdi = faiz = asep = "bolos sekolah"
+print(f"Mas rusdi {rusdi}")
+print(f"Mas faiz {faiz}")
+print(f"Mas asep {asep}")
+
+# kalau ada 2 variabel, terus satunya integer (a) satunya string (b), itu kalau di print(a + b) tidak bisa, harus pakai print(a,b)
